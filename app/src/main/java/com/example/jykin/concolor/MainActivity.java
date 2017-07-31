49d8abd1cbe9d7d364f782eb0762532a0805057b
michael <mocegue8@calstatela.com>
2017-07-30 22:55:34 -0700

    Changed layout to Constraint, Added RGB activity XML

--- app/src/main/java/com/example/jykin/concolor/MainActivity.java
+++ app/src/main/java/com/example/jykin/concolor/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ColorPickerView picker = (ColorPickerView)findViewById(R.id.colorPicker);
+       ColorPickerView picker = (ColorPickerView)findViewById(R.id.colorPicker);
         picker.setColor(0xffff0000);
     }
 
