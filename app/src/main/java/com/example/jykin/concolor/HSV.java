a2a31231bbcdf9748122e7a0fb69692d831ed586
michael <mocegue8@calstatela.edu>
2017-08-01 00:22:41 -0700

    Merge

---
+++ app/src/main/java/com/example/jykin/concolor/HSV.java
@@ -0,0 +1,72 @@
+package com.example.jykin.concolor;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.rarepebble.colorpicker.ColorPickerView;
+
+/**
+ * Created by TheBlah on 7/31/2017.
+ */
+
+public class HSV extends DialogFragment {
+    private ColorPickerView colorPickerView;
+    private Button bCancel, bOk;
+    private int initialColor;
+    private final String TAG = "hsv_fragment";
+
+    public HSV(){
+
+    }
+
+    public static HSV newInstance(int color){
+        HSV newhsv = new HSV();
+
+        Bundle args = new Bundle();
+        args.putInt("initialColor", color);
+        newhsv.setArguments(args);
+
+        return newhsv;
+    }
+
+    public interface OnDialogCloseListener {
+        void closeDialog(int color);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.hsv, container, false);
+        colorPickerView = (ColorPickerView) view.findViewById(R.id.colorPicker);
+        initialColor = getArguments().getInt("initialColor");
+        colorPickerView.setColor(initialColor);
+        bCancel = (Button) view.findViewById(R.id.button_cancel);
+        bCancel.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                HSV.OnDialogCloseListener activity = (HSV.OnDialogCloseListener) getActivity();
+                activity.closeDialog(initialColor);
+                HSV.this.dismiss();
+            }
+        });
+
+        bOk = (Button) view.findViewById(R.id.button_ok);
+        bOk.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                HSV.OnDialogCloseListener activity = (HSV.OnDialogCloseListener) getActivity();
+                activity.closeDialog(colorPickerView.getColor());
+                HSV.this.dismiss();
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/jykin/concolor/MainActivity.java
+++ app/src/main/java/com/example/jykin/concolor/MainActivity.java
@@ -1,20 +1,112 @@
 package com.example.jykin.concolor;
 
+import android.graphics.Color;
+import android.nfc.Tag;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import com.rarepebble.colorpicker.ColorPickerView;
+/**
+ *
+ */
 
-public class MainActivity extends AppCompatActivity {
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class MainActivity extends AppCompatActivity
+    implements HSV.OnDialogCloseListener {
+    private ActionBar actionBar;
+    private int primary, darkPrimary, accent;
+    private Button buttonHSV, buttonPreviewApp, buttonPreviewWeb;
+    private ImageButton ibPrimary, ibAccent, ibDark;
+
+    private TextView et_a, et_r, et_g, et_b;
+    private static final String TAG = "mainactivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       ColorPickerView picker = (ColorPickerView)findViewById(R.id.colorPicker);
-        picker.setColor(0xffff0000);
+        //get ui elements
+        actionBar = getSupportActionBar();
+        primary = ContextCompat.getColor(this, R.color.colorPrimary);
+        darkPrimary = ContextCompat.getColor(this, R.color.colorPrimaryDark);
+        accent = ContextCompat.getColor(this, R.color.colorAccent);
+        //argb edit text views
+        et_a = (TextView) findViewById(R.id.et_a);
+        et_r = (TextView) findViewById(R.id.et_r);
+        et_g = (TextView) findViewById(R.id.et_g);
+        et_b = (TextView) findViewById(R.id.et_b);
+        //hsv button
+        buttonHSV = (Button) findViewById(R.id.HSV_Button);
+        buttonHSV.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //click listener for hsv button
+                //user can only input non decimal numbers
+                //try catch still required for empty edittext case
+                int a, r, g, b;
+                try {
+                    a = Integer.parseInt(et_a.getText().toString());
+                    r = Integer.parseInt(et_r.getText().toString());
+                    g = Integer.parseInt(et_g.getText().toString());
+                    b = Integer.parseInt(et_b.getText().toString());
+                } catch (NumberFormatException e) {
+                    a = 255;
+                    r = g = b = 0;
+                }
+
+                a = (a > 255 || a < 0) ? 255 : a;
+                r = (r > 255 || r < 0) ? 255 : r;
+                g = (g > 255 || g < 0) ? 255 : g;
+                b = (b > 255 || b < 0) ? 255 : b;
+
+                int color = Color.argb(a, r, g, b);
+                FragmentManager fm = getSupportFragmentManager();
+                HSV hsv = HSV.newInstance(color);
+                hsv.show(fm, "hsv_fragment");
+            }
+        });
+        //preview buttons
+        buttonPreviewApp = (Button) findViewById(R.id.b_preview_app);
+        buttonPreviewWeb = (Button) findViewById(R.id.b_preview_web);
+        //color image buttons
+        ibPrimary = (ImageButton) findViewById(R.id.ib_primary);
+        ibDark = (ImageButton) findViewById(R.id.ib_primary_dark);
+        ibAccent = (ImageButton) findViewById(R.id.ib_accent);
     }
 
 
+    @SuppressWarnings("PointlessBitwiseExpression")
+    @Override
+    public void closeDialog(int color) {
+        String hexValue = String.format("#%08X", (0xFFFFFFFF & color));
+        setEditText(color);
+        Log.d(TAG, hexValue);
+    }
+
+    //helper method to set edit text boxes to arbg numbers
+    private void setEditText(int color){
+        int a, r, g, b;
+        a = Color.alpha(color);
+        r = Color.red(color);
+        g = Color.green(color);
+        b = Color.blue(color);
+
+        et_a.setText(Integer.toString(a));
+        et_r.setText(Integer.toString(r));
+        et_g.setText(Integer.toString(g));
+        et_b.setText(Integer.toString(b));
+    }
 }
