431152f0fb2f3f42aa047c9e8810ab5f6d81b33e
michael <mocegue8@calstatela.com>
2017-08-03 01:36:12 -0700

    Auto generate dark/accent, Display color in IB, Long press implemented

--- app/src/main/java/com/example/jykin/concolor/MainActivity.java
+++ app/src/main/java/com/example/jykin/concolor/MainActivity.java
@@ -19,17 +19,19 @@ import android.widget.TextView;
 
 import java.net.URL;
 
+
 /**
  *
  */
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class MainActivity extends AppCompatActivity
-    implements HSV.OnDialogCloseListener, PaletteFragment.OnDialogCloseListener {
+    implements HSV.OnDialogCloseListener, PaletteFragment.OnDialogCloseListener, View.OnLongClickListener {
     private ActionBar actionBar;
     private int primary, darkPrimary, accent;
     private Button buttonHSV, buttonPalette, buttonPreviewApp, buttonPreviewWeb;
     private ImageButton ibPrimary, ibAccent, ibDark;
+    private int sPrimary, sDarkPrimary, sAccent;
 
     private TextView et_a, et_r, et_g, et_b;
     private static final String TAG = "mainactivity";
@@ -44,11 +46,13 @@ public class MainActivity extends AppCompatActivity
         primary = ContextCompat.getColor(this, R.color.colorPrimary);
         darkPrimary = ContextCompat.getColor(this, R.color.colorPrimaryDark);
         accent = ContextCompat.getColor(this, R.color.colorAccent);
+
         //argb edit text views
         et_a = (TextView) findViewById(R.id.et_a);
         et_r = (TextView) findViewById(R.id.et_r);
         et_g = (TextView) findViewById(R.id.et_g);
         et_b = (TextView) findViewById(R.id.et_b);
+
         //hsv button
         buttonHSV = (Button) findViewById(R.id.HSV_Button);
         buttonHSV.setOnClickListener(new View.OnClickListener(){
@@ -75,6 +79,7 @@ public class MainActivity extends AppCompatActivity
         });
         //preview buttons
         buttonPreviewApp = (Button) findViewById(R.id.b_preview_app);
+
         //Web Preview
         buttonPreviewWeb = (Button) findViewById(R.id.b_preview_web);
         buttonPreviewWeb.setOnClickListener(new View.OnClickListener() {
@@ -82,13 +87,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
 
-                int r, g, b;
-                r = Integer.parseInt(et_r.getText().toString());
-                g = Integer.parseInt(et_g.getText().toString());
-                b = Integer.parseInt(et_b.getText().toString());
-                int color = Color.rgb(r, g, b);
-
-                String hexValue = String.format("%06X", (0xFFFFFF & color));
+                String hexValue = String.format("%06X", (0xFFFFFF & argbToColor()));
                 URL url = WebPreview.makeURL(hexValue);
 
                 Uri newsPage = Uri.parse(url.toString());
@@ -101,16 +100,27 @@ public class MainActivity extends AppCompatActivity
 
         //color image buttons
         ibPrimary = (ImageButton) findViewById(R.id.ib_primary);
+        ibPrimary.setOnLongClickListener(this);
         ibDark = (ImageButton) findViewById(R.id.ib_primary_dark);
+        ibDark.setOnLongClickListener(this);
         ibAccent = (ImageButton) findViewById(R.id.ib_accent);
+        ibAccent.setOnLongClickListener(this);
     }
-
 
     @SuppressWarnings("PointlessBitwiseExpression")
     @Override
     public void closeDialog(int color) {
         String hexValue = String.format("#%08X", (0xFFFFFFFF & color));
         setEditText(color);
+
+
+        sPrimary = color;
+        sDarkPrimary = rgbToDarkColor();
+        sAccent = colorToAccent(color);
+
+        ibPrimary.setBackgroundColor(sPrimary);
+        ibDark.setBackgroundColor(sDarkPrimary);
+        ibAccent.setBackgroundColor(sAccent);
         Log.d(TAG, hexValue);
     }
 
@@ -127,6 +137,8 @@ public class MainActivity extends AppCompatActivity
         et_g.setText(Integer.toString(g));
         et_b.setText(Integer.toString(b));
     }
+
+
     public int argbToColor(){
         //user can only input non decimal numbers
         //try catch still required for empty edittext case
@@ -147,5 +159,62 @@ public class MainActivity extends AppCompatActivity
         b = (b > 255 || b < 0) ? 255 : b;
 
         return Color.argb(a, r, g, b);
+    }
+
+    public int rgbToDarkColor(){
+        int r, g, b;
+        //Darkens the color by 50%
+        double darkenShade = 0.5;
+        try {
+            r = Integer.parseInt(et_r.getText().toString());
+            g = Integer.parseInt(et_g.getText().toString());
+            b = Integer.parseInt(et_b.getText().toString());
+        } catch (NumberFormatException e) {
+            r = g = b = 255;
+        }
+
+        r = (int) (((r > 255 || r < 0) ? 255 : r) * darkenShade);
+        g = (int) (((g > 255 || g < 0) ? 255 : g) * darkenShade);
+        b = (int) (((b > 255 || b < 0) ? 255 : b) * darkenShade);
+
+        return Color.rgb(r, g, b);
+    }
+
+    //Changes the hue of the color to get accent. Currently does not work if primary is white or black
+    public int colorToAccent(int color) {
+        float[] hsv = new float[3];
+        Color.RGBToHSV(Color.red(color), Color.green(color),
+                Color.blue(color), hsv);
+        hsv[0] = (hsv[0] + 180) % 360;
+
+        return Color.HSVToColor(hsv);
+    }
+
+    //Saves color and displays on imagebutton when clicked
+    public void ibColorsClick(View view){
+        if (view.getId() == R.id.ib_primary) {
+            sPrimary = argbToColor();
+            ibPrimary.setBackgroundColor(sPrimary);
+        } else if (view.getId() == R.id.ib_primary_dark) {
+            sDarkPrimary = argbToColor();
+            ibDark.setBackgroundColor(sDarkPrimary);
+        } else if (view.getId() == R.id.ib_accent){
+            sAccent = argbToColor();
+            ibAccent.setBackgroundColor(sAccent);
+        }
+    }
+
+
+    //Long Click on imagebutton displays color rgb currently in it
+    @Override
+    public boolean onLongClick(View view) {
+        if (view.getId() == R.id.ib_primary) {
+            setEditText(sPrimary);
+        } else if (view.getId() == R.id.ib_primary_dark) {
+            setEditText(sDarkPrimary);
+        } else if (view.getId() == R.id.ib_accent){
+            setEditText(sAccent);
+        }
+        return true;
     }
 }
