f8b26121bb02b0ac607d63a36e07e80907c70d72
michael <mocegue8@calstatela.edu>
2017-08-04 14:02:32 -0700

    minor changes

--- app/src/main/java/com/example/jykin/concolor/MainActivity.java
+++ app/src/main/java/com/example/jykin/concolor/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity
 
         //Web Preview
         buttonPreviewWeb = (Button) findViewById(R.id.b_preview_web);
-        buttonPreviewApp.setOnClickListener(this);
+        buttonPreviewWeb.setOnClickListener(this);
 
         //color image buttons
         ibPrimary = (ImageButton) findViewById(R.id.ib_primary);
@@ -162,15 +162,6 @@ public class MainActivity extends AppCompatActivity
         Color.RGBToHSV(Color.red(color), Color.green(color),
                 Color.blue(color), hsv);
         hsv[0] = (hsv[0] + 180) % 360;
-        
-         //if color is black or white, the accent would be the opposite.
-        if(hsv[2] == 1.0) {
-            hsv[2] = 0;
-            return Color.HSVToColor(hsv);
-        }else if(hsv[2] == 0){
-            hsv[2] = 1;
-            return Color.HSVToColor(hsv);
-        }
 
         return Color.HSVToColor(hsv);
     }
--- app/src/main/java/com/example/jykin/concolor/WebPreview.java
+++ app/src/main/java/com/example/jykin/concolor/WebPreview.java
@@ -15,21 +15,6 @@ public class WebPreview {
 
     private static  final String BASIC_HTML_PAGE = "http://138.68.240.171/";
 
-    public static URL makeURL(String hexCode){
-        Uri uri = Uri.parse(BASIC_HTML_PAGE).buildUpon()
-                .appendQueryParameter("hex", hexCode).build();
-
-        URL url = null;
-
-        try {
-            url = new URL(uri.toString());
-        }catch(MalformedURLException e){
-            e.printStackTrace();
-        }
-
-        return url;
-    }
-
     // newer version to account for color scheme full.
     public static URL makeURL(String primCol, String darkCol, String acceCol){
         Uri uri = Uri.parse(BASIC_HTML_PAGE).buildUpon()
